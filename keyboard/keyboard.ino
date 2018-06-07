609f7a2a5f74cd1448147b74e05502773c225593
Bob_work <musgravehill@gmail.com>
2018-06-07 22:54:06 +0300

    init

---
+++ keyboard/keyboard.ino
@@ -0,0 +1,15 @@
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  delay(500);
+  Serial.println(sensorValue, DEC);
+}
